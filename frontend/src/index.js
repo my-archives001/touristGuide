11ce504c9d69c74969784700ea9aa317c53e0f95
Ragavendra0604 <2023510031@student.annauniv.edu>
2026-07-27 12:01:29 +0530

    Analytics Features added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals.js';
 import { AuthProvider } from './context/AuthContext.js';
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthProvider>
       <App />
+      <Analytics />
+      <SpeedInsights />
     </AuthProvider>
   </React.StrictMode>
 );
